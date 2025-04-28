e20547b348ff1dd97363987a528762803fe31b07
Srishti-Vashishth <157913014+Srishti-Vashishth@users.noreply.github.com>
2025-04-28 20:55:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI-Powered Medical Chatbot for Intelligent Healthcare Assistance',
+    version= '0.0.0',
+    author= 'SrishtiVashisht',
+    author_email= 'sr9369330000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
